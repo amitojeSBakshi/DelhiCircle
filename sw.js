c227eb86ba74cad140abffefcfc05752095e9670
Amitoje Singh Bakshi <amitojeSBakshi@users.noreply.github.com>
2026-08-15 13:20:29 +0530

    Refresh Delhi Circle board

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿/* Delhi Circle - build 20260814-195508-9772 */
-var CACHE = "delhi-circle-20260814-195508-9772";
+﻿/* Delhi Circle - build 20260815-074714-2464 */
+var CACHE = "delhi-circle-20260815-074714-2464";
 var ASSETS = ["./", "./index.html", "./manifest.json",
               "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
